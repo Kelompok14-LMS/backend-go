0bfe2426e4ab71120cdbc6a258db6ab0bae28eb1
arvinpaundra <arvinpaundra06@gmail.com>
2022-11-24 22:05:07 +0700

    add redis init configuration

--- main.go
+++ main.go
@@ -2,11 +2,12 @@ package main
 
 import (
 	_dbMySQL "github.com/Kelompok14-LMS/backend-go/drivers/mysql"
+	_dbRedis "github.com/Kelompok14-LMS/backend-go/drivers/redis"
 	"github.com/Kelompok14-LMS/backend-go/utils"
 )
 
 func main() {
-	configDB := _dbMySQL.ConfigDB{
+	configMySQL := _dbMySQL.ConfigDB{
 		MYSQL_USERNAME: utils.GetConfig("MYSQL_USERNAME"),
 		MYSQL_PASSWORD: utils.GetConfig("MYSQL_PASSWORD"),
 		MYSQL_HOST:     utils.GetConfig("MYSQL_HOST"),
@@ -14,7 +15,17 @@ func main() {
 		MYSQL_NAME:     utils.GetConfig("MYSQL_NAME"),
 	}
 
-	db := configDB.InitDB()
+	mysqlDB := configMySQL.InitMySQLDatabase()
 
-	_dbMySQL.DBMigrate(db)
+	_dbMySQL.DBMigrate(mysqlDB)
+
+	configRedis := _dbRedis.ConfigDB{
+		REDIS_HOST:     utils.GetConfig("REDIS_HOST"),
+		REDIS_PORT:     utils.GetConfig("REDIS_PORT"),
+		REDIS_PASSWORD: utils.GetConfig("REDIS_PASSWORD"),
+		REDIS_DB:       utils.GetConfig("REDIS_DB"),
+	}
+
+	// TODO USE REDIS CLIENT
+	_ = configRedis.InitRedisDatabase()
 }
