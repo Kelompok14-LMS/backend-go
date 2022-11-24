a345b8ecc59fff496957e547535435de9d693774
arvinpaundra <arvinpaundra06@gmail.com>
2022-11-24 22:06:37 +0700

    add user domain

--- businesses/users/domain.go
+++ businesses/users/domain.go
@@ -1,1 +1,29 @@
 package users
+
+import "time"
+
+type Domain struct {
+	ID        string
+	Email     string
+	Password  string
+	CreatedAt time.Time
+	UpdatedAt time.Time
+}
+
+type Repository interface {
+	Create(userDomain *Domain) error
+	FindAll() (*[]Domain, error)
+	FindByEmail(email string) (*[]Domain, error)
+	FindByID(id string) (*Domain, error)
+	Update(id string, userDomain *Domain) error
+	Delete(id string) error
+}
+
+type Usecase interface {
+	Create(userDomain *Domain) error
+	FindAll() (*[]Domain, error)
+	FindByEmail(email string) (*[]Domain, error)
+	FindByID(id string) (*Domain, error)
+	Update(id string, userDomain *Domain) error
+	Delete(id string) error
+}
