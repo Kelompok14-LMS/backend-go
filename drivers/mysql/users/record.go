5ce45d95017f8b0f04022775d7bfbc37ae80c25e
arvinpaundra <arvinpaundra06@gmail.com>
2022-11-24 22:07:14 +0700

    add user model

--- drivers/mysql/users/record.go
+++ drivers/mysql/users/record.go
@@ -1,1 +1,35 @@
 package users
+
+import (
+	"time"
+
+	"github.com/Kelompok14-LMS/backend-go/businesses/users"
+)
+
+type User struct {
+	ID        string    `gorm:"primaryKey;size:200" json:"id"`
+	Email     string    `gorm:"size:255" json:"email"`
+	Password  string    `gorm:"size:255" json:"password"`
+	CreatedAt time.Time `json:"created_at"`
+	UpdatedAt time.Time `json:"updated_at"`
+}
+
+func FromDomain(domain *users.Domain) *User {
+	return &User{
+		ID:        domain.ID,
+		Email:     domain.Email,
+		Password:  domain.Password,
+		CreatedAt: domain.CreatedAt,
+		UpdatedAt: domain.UpdatedAt,
+	}
+}
+
+func (rec *User) ToDomain() users.Domain {
+	return users.Domain{
+		ID:        rec.ID,
+		Email:     rec.Email,
+		Password:  rec.Password,
+		CreatedAt: rec.CreatedAt,
+		UpdatedAt: rec.UpdatedAt,
+	}
+}
