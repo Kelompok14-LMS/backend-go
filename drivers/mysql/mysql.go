aa8e062393798b8256b4952ef05162824f6b19f5
arvinpaundra <arvinpaundra06@gmail.com>
2022-11-29 14:20:09 +0700

    chore: update database migration

--- drivers/mysql/mysql.go
+++ drivers/mysql/mysql.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"log"
 
+	"github.com/Kelompok14-LMS/backend-go/drivers/mysql/mentees"
 	"github.com/Kelompok14-LMS/backend-go/drivers/mysql/users"
 	"gorm.io/driver/mysql"
 	"gorm.io/gorm"
@@ -40,5 +41,5 @@ func (config *ConfigDB) InitMySQLDatabase() *gorm.DB {
 }
 
 func DBMigrate(db *gorm.DB) {
-	db.AutoMigrate(&users.User{})
+	db.AutoMigrate(&users.User{}, &mentees.Mentee{})
 }
