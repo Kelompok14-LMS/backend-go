799ff9fc0bd1ba4899a54d2ac428725e22b4fd9e
arvinpaundra <arvinpaundra06@gmail.com>
2022-12-03 12:47:16 +0700

    chore: add bucket directory name for cloud storage

--- constants/constants.go
+++ constants/constants.go
@@ -5,4 +5,16 @@ import "time"
 const (
 	// TTL otp redis 5 min
 	TIME_TO_LIVE = 5 * time.Minute
+
+	// Google Cloud Storage base URL
+	STORAGE_URL = "https://storage.googleapis.com"
+
+	// pdf or assignments dir
+	ASSIGNMENTS_DIR = "assignments"
+
+	// videos dir
+	VIDEOS_DIR = "videos"
+
+	// images dir
+	IMAGES_DIR = "images"
 )
