484367003a717590c769f662699727577ca09108
arvinpaundra <arvinpaundra06@gmail.com>
2022-12-04 02:49:17 +0700

    fix: solve tls connection issue and check redis connection

--- drivers/redis/redis.go
+++ drivers/redis/redis.go
@@ -1,7 +1,10 @@
 package redis_driver
 
 import (
+	"context"
+	"crypto/tls"
 	"fmt"
+	"log"
 	"strconv"
 
 	"github.com/go-redis/redis/v8"
@@ -25,10 +28,17 @@ func (config *ConfigDB) InitRedisDatabase() *redis.Client {
 	redisDB, _ := strconv.Atoi(db)
 
 	client := redis.NewClient(&redis.Options{
-		Addr:     address,
-		Password: password,
-		DB:       redisDB,
+		Addr:      address,
+		Password:  password,
+		DB:        redisDB,
+		TLSConfig: &tls.Config{},
 	})
+
+	if _, err := client.Ping(context.Background()).Result(); err != nil {
+		log.Fatalf("error connecting to redis: %v", err)
+	}
+
+	log.Print("connected with redis")
 
 	return client
 }
