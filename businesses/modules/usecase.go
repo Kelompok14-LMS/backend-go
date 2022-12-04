449e4efdf29865c90ee359e6d711d011c7febb5b
arvinpaundra <arvinpaundra06@gmail.com>
2022-12-04 14:15:15 +0700

    feat(course-modules): implements module usecase interface

--- businesses/modules/usecase.go
+++ businesses/modules/usecase.go
@@ -1,1 +1,82 @@
 package modules
+
+import (
+	"github.com/Kelompok14-LMS/backend-go/businesses/courses"
+	"github.com/google/uuid"
+)
+
+type moduleUsecase struct {
+	moduleRepository Repository
+	courseRepository courses.Repository
+}
+
+func NewModuleUsecase(moduleRepository Repository, courseRepository courses.Repository) Usecase {
+	return moduleUsecase{
+		moduleRepository: moduleRepository,
+		courseRepository: courseRepository,
+	}
+}
+
+func (mu moduleUsecase) Create(moduleDomain *Domain) error {
+	if _, err := mu.courseRepository.FindById(moduleDomain.CourseId); err != nil {
+		return err
+	}
+
+	id := uuid.NewString()
+
+	module := Domain{
+		ID:       id,
+		CourseId: moduleDomain.CourseId,
+		Title:    moduleDomain.Title,
+	}
+
+	err := mu.moduleRepository.Create(&module)
+
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func (mu moduleUsecase) FindById(moduleId string) (*Domain, error) {
+	module, err := mu.moduleRepository.FindById(moduleId)
+
+	if err != nil {
+		return nil, err
+	}
+
+	return module, nil
+}
+
+func (mu moduleUsecase) Update(moduleId string, moduleDomain *Domain) error {
+	if _, err := mu.courseRepository.FindById(moduleDomain.CourseId); err != nil {
+		return err
+	}
+
+	if _, err := mu.moduleRepository.FindById(moduleId); err != nil {
+		return err
+	}
+
+	err := mu.moduleRepository.Update(moduleId, moduleDomain)
+
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func (mu moduleUsecase) Delete(moduleId string) error {
+	if _, err := mu.moduleRepository.FindById(moduleId); err != nil {
+		return err
+	}
+
+	err := mu.moduleRepository.Delete(moduleId)
+
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
