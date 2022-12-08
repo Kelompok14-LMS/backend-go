ef863089408d7e316391998a56e01b912ec95281
arvinpaundra <arvinpaundra06@gmail.com>
2022-12-08 22:57:43 +0700

    add field description

--- controllers/courses/response/find_courses.go
+++ controllers/courses/response/find_courses.go
@@ -7,23 +7,25 @@ import (
 )
 
 type FindCourses struct {
-	CourseId  string    `json:"course_id"`
-	Mentor    string    `json:"mentor"`
-	Category  string    `json:"category"`
-	Title     string    `json:"title"`
-	Thumbnail string    `json:"thumbnail"`
-	CreatedAt time.Time `json:"created_at"`
-	UpdatedAt time.Time `json:"updated_at"`
+	CourseId    string    `json:"course_id"`
+	Mentor      string    `json:"mentor"`
+	Category    string    `json:"category"`
+	Title       string    `json:"title"`
+	Description string    `json:"description"`
+	Thumbnail   string    `json:"thumbnail"`
+	CreatedAt   time.Time `json:"created_at"`
+	UpdatedAt   time.Time `json:"updated_at"`
 }
 
 func AllCourses(res *courses.Domain) FindCourses {
 	return FindCourses{
-		CourseId:  res.ID,
-		Mentor:    res.Mentor.Fullname,
-		Category:  res.Category.Name,
-		Title:     res.Title,
-		Thumbnail: res.Thumbnail,
-		CreatedAt: res.CreatedAt,
-		UpdatedAt: res.UpdatedAt,
+		CourseId:    res.ID,
+		Mentor:      res.Mentor.Fullname,
+		Category:    res.Category.Name,
+		Title:       res.Title,
+		Description: res.Description,
+		Thumbnail:   res.Thumbnail,
+		CreatedAt:   res.CreatedAt,
+		UpdatedAt:   res.UpdatedAt,
 	}
 }
