65424f83f625e72da8c4cf4116bc3dffb4596fdd
arvinpaundra <arvinpaundra06@gmail.com>
2022-12-12 00:00:07 +0700

    add full detail of course

---
+++ businesses/detailCourse/domain.go
@@ -0,0 +1,41 @@
+package detail_course
+
+import "time"
+
+type Domain struct {
+	CourseId    string
+	MentorId    string
+	CategoryId  string
+	Title       string
+	Description string
+	Thumbnail   string
+	Category    string
+	Mentor      string
+	Modules     []Module
+	CreatedAt   time.Time
+	UpdatedAt   time.Time
+}
+
+type Module struct {
+	ModuleId  string
+	CourseId  string
+	Title     string
+	Materials []Material
+	CreatedAt time.Time
+	UpdatedAt time.Time
+}
+
+type Material struct {
+	MaterialId  string
+	ModuleId    string
+	Title       string
+	URL         string
+	Description string
+	CreatedAt   time.Time
+	UpdatedAt   time.Time
+}
+
+type Usecase interface {
+	// detail course with modules and materials
+	DetailCourse(courseId string) (*Domain, error)
+}
---
+++ controllers/detailCourse/http.go
@@ -0,0 +1,42 @@
+package detail_course
+
+import (
+	"errors"
+	"net/http"
+
+	detailCourse "github.com/Kelompok14-LMS/backend-go/businesses/detailCourse"
+	"github.com/Kelompok14-LMS/backend-go/controllers/detailCourse/response"
+	"github.com/Kelompok14-LMS/backend-go/helper"
+	"github.com/Kelompok14-LMS/backend-go/pkg"
+	"github.com/labstack/echo/v4"
+)
+
+type DetailCourseController struct {
+	detailCourseUsecase detailCourse.Usecase
+}
+
+func NewDetailCourseController(detailCourseUsecase detailCourse.Usecase) *DetailCourseController {
+	return &DetailCourseController{
+		detailCourseUsecase: detailCourseUsecase,
+	}
+}
+
+func (ctrl *DetailCourseController) HandlerDetailCourse(c echo.Context) error {
+	courseId := c.Param("courseId")
+
+	course, err := ctrl.detailCourseUsecase.DetailCourse(courseId)
+
+	if err != nil {
+		if errors.Is(err, pkg.ErrCourseNotFound) {
+			return c.JSON(http.StatusNotFound, helper.NotFoundResponse(err.Error()))
+		} else if errors.Is(err, pkg.ErrModuleNotFound) {
+			return c.JSON(http.StatusNotFound, helper.NotFoundResponse(err.Error()))
+		} else if errors.Is(err, pkg.ErrMaterialNotFound) {
+			return c.JSON(http.StatusNotFound, helper.NotFoundResponse(err.Error()))
+		} else {
+			return c.JSON(http.StatusInternalServerError, helper.InternalServerErrorResponse(err.Error()))
+		}
+	}
+
+	return c.JSON(http.StatusOK, helper.SuccessResponse("Success get full detail course", response.FullDetailCourse(course)))
+}
