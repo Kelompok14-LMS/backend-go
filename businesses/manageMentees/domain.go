aa27fcaa75727c3e3c1760c701f57b17d8a942b6
arvinpaundra <arvinpaundra06@gmail.com>
2022-12-14 19:55:38 +0700

    fix: linter

--- businesses/manageMentees/domain.go
+++ businesses/manageMentees/domain.go
@@ -1,9 +1,5 @@
 package manage_mentees
 
-type domain struct{}
-
-type Repository interface{}
-
 type Usecase interface {
 	// usecase delete access course mentee
 	DeleteAccess(menteeId string, courseId string) error
