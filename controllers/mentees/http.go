d1b416a80b332eacb3d6cf1493c7129a24b92bbd
Your Name <roihansori12@gmail.com>
2022-12-15 17:15:37 +0700

    feat: add mentee profile

--- controllers/mentees/http.go
+++ controllers/mentees/http.go
@@ -9,16 +9,19 @@ import (
 	"github.com/Kelompok14-LMS/backend-go/controllers/mentees/response"
 	"github.com/Kelompok14-LMS/backend-go/helper"
 	"github.com/Kelompok14-LMS/backend-go/pkg"
+	"github.com/Kelompok14-LMS/backend-go/utils"
 	"github.com/labstack/echo/v4"
 )
 
 type MenteeController struct {
 	menteeUsecase mentees.Usecase
+	jwtConfig     *utils.JWTConfig
 }
 
-func NewMenteeController(menteeUsecase mentees.Usecase) *MenteeController {
+func NewMenteeController(menteeUsecase mentees.Usecase, jwtConfig *utils.JWTConfig) *MenteeController {
 	return &MenteeController{
 		menteeUsecase: menteeUsecase,
+		jwtConfig:     jwtConfig,
 	}
 }
 
@@ -161,4 +164,104 @@ func (ctrl MenteeController) HandlerFindMenteesByCourse(c echo.Context) error {
 	}
 
 	return c.JSON(http.StatusOK, helper.SuccessResponse("Success get all mentees", res))
+}
+
+func (ctrl *MenteeController) HandlerFindByID(c echo.Context) error {
+	var id string = c.Param("menteeId")
+
+	mentee, err := ctrl.menteeUsecase.FindById(id)
+
+	if err != nil {
+		if errors.Is(err, pkg.ErrMenteeNotFound) {
+			return c.JSON(http.StatusNotFound, helper.NotFoundResponse(pkg.ErrMenteeNotFound.Error()))
+		} else if errors.Is(err, pkg.ErrUserNotFound) {
+			return c.JSON(http.StatusNotFound, helper.NotFoundResponse(pkg.ErrUserNotFound.Error()))
+		} else {
+			return c.JSON(http.StatusInternalServerError, helper.InternalServerErrorResponse(err.Error()))
+		}
+	}
+
+	return c.JSON(http.StatusOK, helper.SuccessResponse("Success get Mentee by id", response.FromDomain(mentee)))
+}
+
+func (ctrl *MenteeController) HandlerProfileMentee(c echo.Context) error {
+	token, _ := ctrl.jwtConfig.ExtractToken(c)
+
+	mentee, err := ctrl.menteeUsecase.FindById(token.MenteeId)
+
+	if err != nil {
+		if errors.Is(err, pkg.ErrMenteeNotFound) {
+			return c.JSON(http.StatusNotFound, helper.NotFoundResponse(pkg.ErrMenteeNotFound.Error()))
+		} else if errors.Is(err, pkg.ErrUserNotFound) {
+			return c.JSON(http.StatusNotFound, helper.NotFoundResponse(pkg.ErrUserNotFound.Error()))
+		} else {
+			return c.JSON(http.StatusInternalServerError, helper.InternalServerErrorResponse(err.Error()))
+		}
+	}
+
+	return c.JSON(http.StatusOK, helper.SuccessResponse("Success get Mentee by id", response.FromDomain(mentee)))
+}
+
+func (ctrl *MenteeController) HandlerFindAll(c echo.Context) error {
+
+	mentees, err := ctrl.menteeUsecase.FindAll()
+
+	allMentees := []response.FindAllMentees{}
+
+	for _, mentee := range *mentees {
+		allMentees = append(allMentees, response.AllMentees(&mentee))
+	}
+
+	if err != nil {
+		if errors.Is(err, pkg.ErrMenteeNotFound) {
+			return c.JSON(http.StatusNotFound, helper.NotFoundResponse(pkg.ErrMenteeNotFound.Error()))
+		} else if errors.Is(err, pkg.ErrUserNotFound) {
+			return c.JSON(http.StatusNotFound, helper.NotFoundResponse(pkg.ErrUserNotFound.Error()))
+		} else {
+			return c.JSON(http.StatusInternalServerError, helper.InternalServerErrorResponse(err.Error()))
+		}
+	}
+
+	return c.JSON(http.StatusOK, helper.SuccessResponse("Success get all mentor ", allMentees))
+}
+
+func (ctrl *MenteeController) HandlerUpdateProfile(c echo.Context) error {
+	menteeInput := request.MenteeUpdateProfile{}
+
+	menteeId := c.Param("menteeId")
+
+	ProfilePictureFile, _ := c.FormFile("profile_picture")
+
+	if ProfilePictureFile != nil {
+		menteeInput.ProfilePictureFile = ProfilePictureFile
+
+		if err := c.Bind(&menteeInput); err != nil {
+			return c.JSON(http.StatusBadRequest, helper.BadRequestResponse(pkg.ErrInvalidRequest.Error()))
+		}
+	} else {
+		menteeInput.Fullname = c.FormValue("fullname")
+		menteeInput.Phone = c.FormValue("phone")
+		menteeInput.BirthDate = c.FormValue("birth_date")
+		menteeInput.ProfilePictureFile = nil
+	}
+
+	if err := menteeInput.Validate(); err != nil {
+		return c.JSON(http.StatusBadRequest, helper.BadRequestResponse(err.Error()))
+	}
+
+	err := ctrl.menteeUsecase.Update(menteeId, menteeInput.ToDomain())
+
+	if err != nil {
+		if errors.Is(err, pkg.ErrInvalidRequest) {
+			return c.JSON(http.StatusBadRequest, helper.BadRequestResponse(pkg.ErrInvalidRequest.Error()))
+		} else if errors.Is(err, pkg.ErrMenteeNotFound) {
+			return c.JSON(http.StatusBadRequest, helper.BadRequestResponse(pkg.ErrMenteeNotFound.Error()))
+		} else if errors.Is(err, pkg.ErrUserNotFound) {
+			return c.JSON(http.StatusNotFound, helper.NotFoundResponse(pkg.ErrUserNotFound.Error()))
+		} else {
+			return c.JSON(http.StatusInternalServerError, helper.InternalServerErrorResponse(err.Error()))
+		}
+	}
+
+	return c.JSON(http.StatusOK, helper.SuccessResponse("Success update profile", nil))
 }
