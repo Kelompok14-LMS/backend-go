afe1f4eac6853ea69f97aeffb83c4b60b49e894b
arvinpaundra <arvinpaundra06@gmail.com>
2022-12-19 13:13:21 +0700

    feat(mentee-progresses): return mentee progress in detail course

--- controllers/detailCourse/response/detail_course_enrolled.go
+++ controllers/detailCourse/response/detail_course_enrolled.go
@@ -1,9 +1,63 @@
 package response
 
-import detailCourse "github.com/Kelompok14-LMS/backend-go/businesses/detailCourse"
+import (
+	"time"
 
-func FullDetailCourseEnrolled(domain *detailCourse.Domain) *Course {
-	modules := make([]Module, len(domain.Modules))
+	detailCourse "github.com/Kelompok14-LMS/backend-go/businesses/detailCourse"
+)
+
+type CourseEnrolled struct {
+	CourseId      string             `json:"course_id"`
+	CategoryId    string             `json:"category_id"`
+	MentorId      string             `json:"mentor_id"`
+	Mentor        string             `json:"mentor"`
+	Category      string             `json:"category"`
+	Title         string             `json:"title"`
+	Description   string             `json:"description"`
+	Thumbnail     string             `json:"thumbnail"`
+	TotalMaterial int64              `json:"total_materials"`
+	Progress      int64              `json:"progress"`
+	TotalReviews  int                `json:"total_reviews"`
+	Rating        float32            `json:"rating"`
+	Modules       []ModuleEnrolled   `json:"modules"`
+	Assignment    AssignmentEnrolled `json:"assignment"`
+	CreatedAt     time.Time          `json:"created_at"`
+	UpdatedAt     time.Time          `json:"updated_at"`
+}
+
+type AssignmentEnrolled struct {
+	AssignmentID string    `json:"assignment_id"`
+	CourseId     string    `json:"course_id"`
+	Title        string    `json:"title"`
+	Description  string    `json:"description"`
+	Completed    bool      `json:"completed"`
+	CreatedAt    time.Time `json:"created_at"`
+	UpdatedAt    time.Time `json:"updated_at"`
+}
+
+type ModuleEnrolled struct {
+	ModuleId    string             `json:"module_id"`
+	CourseId    string             `json:"course_id"`
+	Title       string             `json:"title"`
+	Description string             `json:"description"`
+	Materials   []MaterialEnrolled `json:"materials"`
+	CreatedAt   time.Time          `json:"created_at"`
+	UpdatedAt   time.Time          `json:"updated_at"`
+}
+
+type MaterialEnrolled struct {
+	MaterialId  string    `json:"material_id"`
+	ModuleId    string    `json:"module_id"`
+	Title       string    `json:"title"`
+	URL         string    `json:"url"`
+	Description string    `json:"description"`
+	Completed   bool      `json:"completed"`
+	CreatedAt   time.Time `json:"created_at"`
+	UpdatedAt   time.Time `json:"updated_at"`
+}
+
+func FullDetailCourseEnrolled(domain *detailCourse.Domain) *CourseEnrolled {
+	modules := make([]ModuleEnrolled, len(domain.Modules))
 
 	for i, module := range domain.Modules {
 		modules[i].ModuleId = module.ModuleId
@@ -15,7 +69,7 @@ func FullDetailCourseEnrolled(domain *detailCourse.Domain) *Course {
 	}
 
 	for i, module := range modules {
-		module.Materials = make([]Material, len(domain.Modules[i].Materials))
+		module.Materials = make([]MaterialEnrolled, len(domain.Modules[i].Materials))
 
 		for j, material := range domain.Modules[i].Materials {
 			if module.ModuleId == material.ModuleId {
@@ -33,29 +87,32 @@ func FullDetailCourseEnrolled(domain *detailCourse.Domain) *Course {
 		}
 	}
 
-	assignment := Assignment{
+	assignment := AssignmentEnrolled{
 		AssignmentID: domain.Assignment.ID,
 		CourseId:     domain.Assignment.CourseId,
 		Title:        domain.Assignment.Title,
 		Description:  domain.Assignment.Description,
+		Completed:    domain.Assignment.Completed,
 		CreatedAt:    domain.Assignment.CreatedAt,
 		UpdatedAt:    domain.Assignment.UpdatedAt,
 	}
 
-	return &Course{
-		CourseId:     domain.CourseId,
-		CategoryId:   domain.CategoryId,
-		MentorId:     domain.MentorId,
-		Mentor:       domain.Mentor,
-		Category:     domain.Category,
-		Title:        domain.Title,
-		Description:  domain.Description,
-		Thumbnail:    domain.Thumbnail,
-		TotalReviews: domain.TotalReviews,
-		Rating:       domain.Rating,
-		Modules:      modules,
-		Assignment:   assignment,
-		CreatedAt:    domain.CreatedAt,
-		UpdatedAt:    domain.UpdatedAt,
+	return &CourseEnrolled{
+		CourseId:      domain.CourseId,
+		CategoryId:    domain.CategoryId,
+		MentorId:      domain.MentorId,
+		Mentor:        domain.Mentor,
+		Category:      domain.Category,
+		Title:         domain.Title,
+		Description:   domain.Description,
+		Thumbnail:     domain.Thumbnail,
+		TotalMaterial: domain.TotalMaterials,
+		Progress:      domain.Progress,
+		TotalReviews:  domain.TotalReviews,
+		Rating:        domain.Rating,
+		Modules:       modules,
+		Assignment:    assignment,
+		CreatedAt:     domain.CreatedAt,
+		UpdatedAt:     domain.UpdatedAt,
 	}
 }
